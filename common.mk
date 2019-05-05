84947a3457b70bea2ea3e292cbeb7ee9bab23c9a
lee.wp14 <lee.wp14@gmail.com>
2019-05-05 09:10:23 +0000

    galaxys2-common: remove live wallpapers, Snap
    Change-Id: I1b3856c8c579cce5be05bfecbf8de3280d6477f6

--- common.mk
+++ common.mk
@@ -62,8 +62,7 @@ PRODUCT_COPY_FILES += \
 
 # Packages
 PRODUCT_PACKAGES := \
-    com.android.future.usb.accessory \
-    Snap
+    com.android.future.usb.accessory
 
 # USB
 PRODUCT_PACKAGES += \
@@ -123,7 +122,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl-legacy \
     camera.device@1.0-impl-legacy \
     camera.smdk4210 \
-    Snap \
     libstagefright-shim
 
 PRODUCT_COPY_FILES += \
@@ -217,14 +215,7 @@ PRODUCT_PACKAGES += \
 
 # Live Wallpapers
 PRODUCT_PACKAGES += \
-    Galaxy4 \
-    HoloSpiralWallpaper \
-    LiveWallpapers \
     LiveWallpapersPicker \
-    MagicSmokeWallpapers \
-    NoiseField \
-    PhaseBeam \
-    VisualizationWallpapers \
     librs_jni
 
 # Wifi
