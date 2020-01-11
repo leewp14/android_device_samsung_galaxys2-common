46957b615f6d414be9e54e8d4002dd727f0443ed
Dominggoes Isakh <drjisakh@gmail.com>
2020-01-11 22:55:37 +0100

    galaxys2-common: Use Lineage's legacy Wifi interface
    Change-Id: Ic6f2f7073b2c23a05720f526f80646e67b44fd02

--- common.mk
+++ common.mk
@@ -235,7 +235,7 @@ PRODUCT_PACKAGES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi@1.0-service.legacy \
     wificond \
     hostapd \
     libwpa_client \
