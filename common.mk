c7057f4ea74bbe2b84e0fd0d0d67daab3fefc244
leewp14 <lee.wp14@gmail.com>
2020-02-17 15:42:00 +0800

    galaxys2-common: build old WallpaperPicker
    New one lags a lot on our poor old device.
    
    Change-Id: Icf2a48f9c9fc56aa3e6675157b0343e811d8c396

--- common.mk
+++ common.mk
@@ -206,6 +206,10 @@ PRODUCT_PACKAGES += \
     fsck.f2fs \
     mkfs.f2fs
 
+# Legacy WallpaperPicker
+PRODUCT_PACKAGES += \
+     WallpaperPicker
+
 # Live Wallpapers
 PRODUCT_PACKAGES += \
     LiveWallpapersPicker \
