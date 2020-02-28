9d353ce6def79bda7b769c0fa3d5aa08ea1fcf4a
leewp14 <lee.wp14@gmail.com>
2020-02-28 08:50:52 +0800

    galaxys2-common: Enable full dexpreopt

--- BoardCommonConfig.mk
+++ BoardCommonConfig.mk
@@ -73,11 +73,12 @@ MALLOC_SVELTE := true
 # Dexpreopt
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
+    ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := false
       WITH_DEXPREOPT := true
+    endif
   endif
 endif
-WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY ?= true
-PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI
 
 # Filesystem
 TARGET_USERIMAGES_USE_EXT4 := true
