1196fa5ef7b7fe1d78de0be879b6b95a0fa71e38
leewp14 <lee.wp14@gmail.com>
2020-02-28 08:35:11 +0800

    galaxys2-common: dex2oat: use 2 cores

--- system_prop.mk
+++ system_prop.mk
@@ -2,5 +2,5 @@
 # ART
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dex2oat-filter=speed \
-    dalvik.vm.image-dex2oat-filter=speed
-
+    dalvik.vm.image-dex2oat-filter=speed \
+    ro.sys.fw.dex2oat_thread_count=2
