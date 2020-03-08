0211192168dc3acee7cd65ffb87700f38d39502d
leewp14 <lee.wp14@gmail.com>
2020-03-08 16:22:34 +0800

    galaxys2-common: force OpenGL renderer
    https://github.com/CustomROMs/android_device_samsung_smdk4412-common/commit/749f84ee60aae11ba43d05941785fc3dbf1e87a3
    
    Apart from that, I noticed implementation unsupported error from skia.
    
    03-08 16:01:07.436  2861  2861 D skia    : --- Failed to create image decoder with message 'unimplemented'

--- system_prop.mk
+++ system_prop.mk
@@ -4,3 +4,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dex2oat-filter=speed \
     dalvik.vm.image-dex2oat-filter=speed \
     ro.sys.fw.dex2oat_thread_count=2
+
+# HWUI
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.hwui.renderer=opengl
