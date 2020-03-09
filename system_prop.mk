6a1bfe37eb97ff58689355e8037b857d40af8944
leewp14 <lee.wp14@gmail.com>
2020-03-10 00:42:16 +0800

    galaxys2-common: Enable GL comp backpressure
    Enable backpressure for client composition to avoid jank due to HWC queue stuffing.
    
    https: //github.com/CustomROMs/android_device_samsung_smdk4412-common/commit/ae1c99d7a05cec1882393a7ea9e7442f9fdc176f
    Change-Id: I35db634c8c5db6dc320a6071c8d45682a46e5ca9

--- system_prop.mk
+++ system_prop.mk
@@ -4,3 +4,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dex2oat-filter=speed \
     dalvik.vm.image-dex2oat-filter=speed \
     ro.sys.fw.dex2oat_thread_count=2
+
+# SurfaceFlinger
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.sf.latch_unsignaled=1 \
+    debug.sf.disable_backpressure=1
