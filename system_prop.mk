e32ebc4e5d25ef468c4bcb344c567d1311e6aa96
leewp14 <lee.wp14@gmail.com>
2020-03-10 23:17:22 +0800

    Revert "galaxys2-common: force OpenGL renderer"
    There are something wrong with the rendering of graphical elements when forcing OpenGL. Reverting to Skia for the time being.
    Case 1: Visual artifacts detected at the bottom left button of Firefox Lite
    Case 2: On DotOS, the battery meter in settings is rendered as a quadricle instead of rounded shape.
    
    This reverts commit 0211192168dc3acee7cd65ffb87700f38d39502d.

--- system_prop.mk
+++ system_prop.mk
@@ -5,10 +5,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.image-dex2oat-filter=speed \
     ro.sys.fw.dex2oat_thread_count=2
 
-# HWUI
-PRODUCT_PROPERTY_OVERRIDES += \
-    debug.hwui.renderer=opengl
-
 # SurfaceFlinger
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.latch_unsignaled=1 \
