9210c1dc9c92d8a015211ec46d34b2ca33f4a1fa
leewp14 <lee.wp14@gmail.com>
2020-03-15 03:26:46 +0800

    Revert "galaxys2-common: Dexopt: Add debug-info"
    This reverts commit a0bcead15ed3dfbb9eb607a0d82d42cd2969fb19.

--- system_prop_debug.mk
+++ system_prop_debug.mk
@@ -2,5 +2,4 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.secure=0 \
     ro.adb.secure=0 \
     ro.debuggable=1 \
-    persist.service.adb.enable=1 \
-    debug.generate-debug-info=1
+    persist.service.adb.enable=1
